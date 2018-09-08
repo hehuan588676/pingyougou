76f5583bf6e2b4d9731f3dbb6697d38d780ddd70
admin <747465230@qq.com>
2018-09-08 15:07:54 +0800

    添加一条你好呀

--- git-test/src/com/itheima/Hello.java
+++ git-test/src/com/itheima/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println("hello-git");
         System.out.println("hello-git");
         System.out.println("hello-git");
+        System.out.println("你好呀");
     }
 }
